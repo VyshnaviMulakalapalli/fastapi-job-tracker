64c9b9a5e7fbe954ccace7de3483ffbbdcb88057
VyshnaviMulakalapalli <vm1096@usnh.edu>
2025-03-04 16:08:08 -0500

    implement generation fo verification codes

--- auth.py
+++ auth.py
@@ -7,11 +7,12 @@ from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from models import User
 from config import SessionLocal
+import secrets
+import os
+from dotenv import load_dotenv
 
-# Constants
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"  
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+load_dotenv()
+
 
 # Password hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -43,7 +44,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, os.getenv("SECRET_KEY"), algorithm=os.getenv("ALGORITHM"))
     return encoded_jwt
 
 # Get current user from token
@@ -54,7 +55,7 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, os.getenv("SECRET_KEY"), algorithms=[os.getenv("ALGORITHM")])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
@@ -68,3 +69,6 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
 # Get user by username
 def get_user(db: Session, username: str):
     return db.query(User).filter(User.username == username).first()
+
+def generate_verification_token():
+    return secrets.token_urlsafe(16)
